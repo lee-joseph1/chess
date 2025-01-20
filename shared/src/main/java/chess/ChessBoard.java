6d5e0d2fc45d6db35409b4349153066a0ac4bf62
Joseph Lee <jrlee803@byu.edu>
2025-01-20 13:22:48 -0700

    ChessBoard completed (I think? Need to figure out how to actually run the tests ._.)

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,8 +8,10 @@ package chess;
  */
 public class ChessBoard {
 
+    private ChessPiece[][] board;
+
     public ChessBoard() {
-        //board = //array of pieces 8x8 remember move +-1 to account 0 indexing
+        board = new ChessPiece[8][8];
     }
 
     /**
@@ -19,7 +21,10 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getColumn() - 1][position.getRow() - 1] = piece;
+        //REMEMBER COL BEFORE ROW i.e. (k)night h3 file h rank 3
+        //REMEMBER INDEXING FROM 0 so add pieces from 1-8
+        //throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -30,7 +35,8 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getColumn() - 1][position.getRow() - 1];
+        //throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -38,8 +44,29 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
-        //for space on board
-            //add null to space
+        //throw new RuntimeException("Not implemented");
+        //wipe board by creating a new one instead
+        board = new ChessPiece[8][8];
+        //set pawns
+        for (int col = 1; col <= 8; col++) {
+            addPiece(new ChessPosition(col, 2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            addPiece(new ChessPosition(col, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
+        for (int col : new int[]{1, 8}) {
+            addPiece(new ChessPosition(col, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+            addPiece(new ChessPosition(col, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+        }
+        for (int col : new int[]{2, 7}) {
+            addPiece(new ChessPosition(col, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+            addPiece(new ChessPosition(col, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        }
+        for (int col : new int[]{3, 6}) {
+            addPiece(new ChessPosition(col, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+            addPiece(new ChessPosition(col, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        }
+        addPiece(new ChessPosition(4, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(4, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(5, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(5, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
     }
 }
