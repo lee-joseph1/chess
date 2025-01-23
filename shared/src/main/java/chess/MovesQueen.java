24dd95392fcbba0c577f31c3c40d5850076a699a
Joseph Lee <jrlee803@byu.edu>
2025-01-23 00:17:52 -0700

    queen moves added

--- shared/src/main/java/chess/MovesQueen.java
+++ shared/src/main/java/chess/MovesQueen.java
@@ -1,9 +1,18 @@
 package chess;
 
-public class MovesQueen {
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MovesQueen extends MoveCalculator{
     //get valid bishop moves
     //get valid rook moves
     //compile list
     //run all validity checks on each
     //compile final list
+    public static Collection<ChessMove> getMoves(ChessBoard board, ChessPosition pos) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        moves.addAll(MovesRook.getMoves(board, pos));
+        moves.addAll(MovesBishop.getMoves(board, pos));
+        return moves;
+    }
 }
