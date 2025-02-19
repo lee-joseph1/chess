5d45a4b27a98ca5991d62eebb49a6accc10384aa
Joseph Lee <jrlee803@byu.edu>
2025-02-18 23:32:39 -0700

    completed server setup, working

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,10 +3,13 @@ import com.google.gson.Gson;
 import spark.*;
 import java.util.*;
 //import chess.server.src.main.java.server.Server;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+        Server server = new Server();
+        server.run(8080);
     }
 }
