b82d8640cbbf34eff4fb0b3d4ccefab9813480a8
Joseph Lee <jrlee803@byu.edu>
2025-02-19 16:37:33 -0700

    record classes completed

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,6 +11,7 @@ public class Server {
 
         // Register your endpoints and handle exceptions here.
 
+
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
