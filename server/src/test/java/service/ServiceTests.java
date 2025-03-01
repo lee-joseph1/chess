fdebfb4c35c119e1f9473d15a91c82916626ba5f
Joseph Lee <jrlee803@byu.edu>
2025-02-28 18:18:09 -0700

    code quality, hopefully

--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -36,45 +36,45 @@ public class ServiceTests {
 
     @Test
     public void failRegister() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user1", "password", "e@mail.mail");
         userService.register(registerRequest);
-        RegisterRequest register2Request = new RegisterRequest("user", "p2", "e@mail.mail");
+        RegisterRequest register2Request = new RegisterRequest("user1", "p2", "e@mail.mail");
         assertThrows(DataAccessException.class, () -> userService.register(register2Request));
     }
 
     @Test
     public void clearUser() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user2", "password", "e@mail.mail");
         userService.register(registerRequest);
         userService.clear();
-        assertNull(userDao.getUserByUsername("user"));
+        assertNull(userDao.getUserByUsername("user2"));
     }
 
     @Test
     public void passLogin() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user3", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "password");
+        LoginRequest loginRequest = new LoginRequest("user3", "password");
         LoginResponse loginResponse = authService.login(loginRequest);
         assertNotNull(loginResponse);
         assertNotNull(loginResponse.authToken());
         assertNotNull(authDao.getAuthByToken(loginResponse.authToken()));
-        assertEquals("user", loginResponse.username());
+        assertEquals("user3", loginResponse.username());
     }
 
     @Test
     public void failLogin() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user4", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "wrong");
+        LoginRequest loginRequest = new LoginRequest("user4", "wrong");
         assertThrows(DataAccessException.class, () -> authService.login(loginRequest));
     }
 
     @Test
     public void passLogout() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user5", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "password");
+        LoginRequest loginRequest = new LoginRequest("user5", "password");
         LoginResponse loginResponse = authService.login(loginRequest);
         LogoutRequest logoutRequest = new LogoutRequest(loginResponse.authToken());
         LogoutResponse logoutResponse = authService.logout(logoutRequest);
@@ -84,9 +84,9 @@ public class ServiceTests {
 
     @Test
     public void failLogout() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user6", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "password");
+        LoginRequest loginRequest = new LoginRequest("user6", "password");
         authService.login(loginRequest);
         LogoutRequest logoutRequest = new LogoutRequest("wrong");
         assertThrows(DataAccessException.class, () -> authService.logout(logoutRequest));
@@ -94,9 +94,9 @@ public class ServiceTests {
 
     @Test
     public void clearAuth() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user7", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "password");
+        LoginRequest loginRequest = new LoginRequest("user7", "password");
         LoginResponse loginResponse = authService.login(loginRequest);
         authService.clear();
         assertNull(authDao.getAuthByToken(loginResponse.authToken()));
@@ -104,9 +104,9 @@ public class ServiceTests {
 
     @Test
     public void passCreate() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user8", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "password");
+        LoginRequest loginRequest = new LoginRequest("user8", "password");
         LoginResponse loginResponse = authService.login(loginRequest);
         CreateRequest createRequest = new CreateRequest("name");
         CreateResponse createResponse = gameService.create(createRequest, loginResponse.authToken());
@@ -116,47 +116,47 @@ public class ServiceTests {
 
     @Test
     public void failCreate() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user9", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "password");
+        LoginRequest loginRequest = new LoginRequest("user9", "password");
         authService.login(loginRequest);
-        CreateRequest createRequest = new CreateRequest("name");
+        CreateRequest createRequest = new CreateRequest("name1");
         assertThrows(DataAccessException.class, () -> gameService.create(createRequest, "wrong"));
     }
 
     @Test
     public void passJoin() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user0", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "password");
+        LoginRequest loginRequest = new LoginRequest("user0", "password");
         LoginResponse loginResponse = authService.login(loginRequest);
-        CreateRequest createRequest = new CreateRequest("name");
+        CreateRequest createRequest = new CreateRequest("name2");
         CreateResponse createResponse = gameService.create(createRequest, loginResponse.authToken());
         assertNotNull(createRequest);
-        assertEquals("name", gameDao.getGameByID(createResponse.gameID()).gameName());
+        assertEquals("name2", gameDao.getGameByID(createResponse.gameID()).gameName());
     }
 
     @Test
     public void failJoin() throws Exception {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user-", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "password");
+        LoginRequest loginRequest = new LoginRequest("user-", "password");
         LoginResponse loginResponse = authService.login(loginRequest);
-        CreateRequest createRequest = new CreateRequest("name");
+        CreateRequest createRequest = new CreateRequest("name3");
         CreateResponse createResponse = gameService.create(createRequest, loginResponse.authToken());
         JoinRequest joinRequest = new JoinRequest("WHITE", createResponse.gameID());
         JoinResponse joinResponse = gameService.join(joinRequest, loginResponse.authToken());
         assertNotNull(joinResponse);
-        assertEquals("user", gameDao.getGameByID(createResponse.gameID()).whiteUsername());
+        assertEquals("user-", gameDao.getGameByID(createResponse.gameID()).whiteUsername());
     }
 
     @Test
     public void passList() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user=", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "password");
+        LoginRequest loginRequest = new LoginRequest("user=", "password");
         LoginResponse loginResponse = authService.login(loginRequest);
-        CreateRequest createRequest = new CreateRequest("name");
+        CreateRequest createRequest = new CreateRequest("name4");
         gameService.create(createRequest, loginResponse.authToken());
         ListResponse listResponse = gameService.list(loginResponse.authToken());
         assertNotNull(listResponse);
@@ -165,22 +165,22 @@ public class ServiceTests {
 
     @Test
     public void failList() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("use", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "password");
+        LoginRequest loginRequest = new LoginRequest("use", "password");
         LoginResponse loginResponse = authService.login(loginRequest);
-        CreateRequest createRequest = new CreateRequest("name");
+        CreateRequest createRequest = new CreateRequest("name5");
         gameService.create(createRequest, loginResponse.authToken());
         assertThrows(DataAccessException.class, () -> gameService.list("wrong"));
     }
 
     @Test
     public void clearGame() throws DataAccessException {
-        RegisterRequest registerRequest = new RegisterRequest("user", "password", "e@mail.mail");
+        RegisterRequest registerRequest = new RegisterRequest("user`", "password", "e@mail.mail");
         userService.register(registerRequest);
-        LoginRequest loginRequest = new LoginRequest("user", "password");
+        LoginRequest loginRequest = new LoginRequest("user`", "password");
         LoginResponse loginResponse = authService.login(loginRequest);
-        CreateRequest createRequest = new CreateRequest("name");
+        CreateRequest createRequest = new CreateRequest("name6");
         CreateResponse createResponse = gameService.create(createRequest, loginResponse.authToken());
         ListResponse listResponse = gameService.list(loginResponse.authToken());
         assertFalse(listResponse.games().isEmpty());
